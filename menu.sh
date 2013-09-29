26b36718b0f5e17f1c3b55bd2ed40da262992280
Osman Makal <osman@ni.net.tr>
2013-09-29 15:09:47 +0300

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+clear
+options=("Birinci Secenek" "Ikinci Secenek" "Ucuncu Secenek" "Dorduncu Secenek")
+menu() {
+    echo "Secenek Listesi:"
+    for i in ${!options[@]}; do 
+        printf "%3d%s) %s\n" $((i+1)) "${secenek[i]:- }" "${options[i]}"
+    done
+    [[ "$msg" ]] && echo "$msg"; :
+}
+uzunmesaj="Secmeye devam et veya ENTER tuslayarak secimi bitir.: "
+while menu && read -rp "$uzunmesaj" num && [[ "$num" ]]; do
+    [[ "$num" != *[![:digit:]]* ]] && (( num > 0 && num <= ${#options[@]} )) || {
+        msg="!!Menu icerisinde boyle bir secenek bulunmuyor!! $num"; continue; clear
+    }
+    ((num--)); msg="${options[num]} ${secenek[num]:+un} secildi."
+    [[ "${secenek[num]}" ]] && secenek[num]="" || secenek[num]="+"
+	clear
+done
+printf "Yapilan Secimler Sunlardir:"; msg=" nothing"
+for i in ${!options[@]}; do 
+    [[ "${secenek[i]}" ]] && { printf " %s" "${options[i]}"; msg=""; }
+done
+echo "$msg"
